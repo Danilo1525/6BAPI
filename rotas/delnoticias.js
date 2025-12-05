657d24408aa395bf1a6d24722603ea1ae0e2bb7b
Danilo Martinez <126427304+Danilo1525@users.noreply.github.com>
2025-12-05 09:14:19 -0300

    Refactor delete news route response format
    Updated response messages for delete operation.

--- rotas/delnoticias.js
+++ rotas/delnoticias.js
@@ -6,10 +6,11 @@ module.exports = (app) => {
             const resultado = await app.DBClient.db('portalnoticias')
                 .collection('noticias')
                 .deleteOne({_id: id})
-            res.status(200).send("Usuário apagado")
+            res.status(200).json{ status: 1, msg: "Notícia apagada com sucesso" }
         } catch (error) {
-            res.status(400).send("Erro ao apagar o usuário: " + error)
+            res.status(400).json({ status: 0, msg: "Erro ao apagar notícia" })
         }
     })
 
 }
+
