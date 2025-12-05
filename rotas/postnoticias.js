1dcf838112b9c538e9b3b55c6a3cbbce9fdecc78
Danilo Martinez <126427304+Danilo1525@users.noreply.github.com>
2025-12-05 08:29:28 -0300

    Enhance postnoticias route with date formatting
    Updated the date formatting to use locale string for 'datahoracadastro' and improved error handling.

--- rotas/postnoticias.js
+++ rotas/postnoticias.js
@@ -4,21 +4,22 @@ module.exports = (app) => {
             const titulonoticia = req.body.titulonoticia
             const conteudonoticia = req.body.conteudonoticia
             const tiponoticia = req.body.tiponoticia
-            await app.DBClient.connect(); //aqui o erro
+            await app.DBClient.connect();
             const resultado = await app.DBClient.db('portalnoticias')
                 .collection('noticias')
                 .insertOne({
                     titulonoticia: titulonoticia,
                     conteudonoticia: conteudonoticia,
                     tiponoticia: tiponoticia,
-                    datahoracadastro: new Date()
+                    datahoracadastro: new Date().toLocaleString('pt-BR', { timeZone: 'America/Cuiaba' })
                 })
             if (!resultado.acknowledged) {
                 res.json({ status: 0 })
             }
             res.json({ status: 1 })
+            // res.status(200).send("Notícia Cadastrada")
         } catch (error) {
-            res.json({ status: 0 })
+            res.status(400).send("Não foi possível cadastrar a notícia")
         }
     })
 }
