cf286285b0e88910db6a44c98e2bb31d4101dc1b
Danilo Martinez <126427304+Danilo1525@users.noreply.github.com>
2025-12-05 09:19:15 -0300

    Update delnoticias.js

--- rotas/delnoticias.js
+++ rotas/delnoticias.js
@@ -6,11 +6,12 @@ module.exports = (app) => {
             const resultado = await app.DBClient.db('portalnoticias')
                 .collection('noticias')
                 .deleteOne({_id: id})
-            res.status(200).json{ status: 1, msg: "Notícia apagada com sucesso" }
+            res.status(200).json({ status: 1, msg: "Notícia apagada com sucesso" })
         } catch (error) {
             res.status(400).json({ status: 0, msg: "Erro ao apagar notícia" })
         }
     })
 
 }
+
 
